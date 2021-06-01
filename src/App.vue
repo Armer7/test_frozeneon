4880e1386c83e8f253c842a334f7bb6cb67a0e4a
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-06-01 22:32:28 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container class="p-4">
+    <Search @getSearchText="getSearchText" />
+    <BlockResult
+      :cardsData="data"
+      :question="question"
+      @showModal="showModal"
+    />
+    <MoreInfoModal
+      v-if="modalPrepareData.modalToggle"
+      :modalData="detailModal"
+      @closeModal="closeModal"
+    />
+    <Pagination
+      v-if="countItems > 0"
+      :paginationData="{ currentPage: page, rows: countItems, perPage: size }"
+      @getPage="getPage"
+    />
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Search from '@/components/Search';
+import BlockResult from '@/components/BlockResult';
+import MoreInfoModal from '@/components/MoreInfoModal';
+import RequestDataGet from '@/module/requestDataGet';
+import Pagination from '@/components/Pagination';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Pagination, MoreInfoModal, BlockResult, Search },
+  data() {
+    return {
+      question: '',
+      urlSearch: 'https://registry.npmjs.org/-/v1/search',
+      urlDetail: 'https://data.jsdelivr.com/v1',
+      data: {},
+      size: 10,
+      page: 1,
+      modalPrepareData: {
+        name: '',
+        version: '',
+        itemId: null,
+        modalToggle: false,
+      },
+      detailModal: {},
+      countItems: 0,
+    };
+  },
+
+  watch: {
+    async question() {
+      this.data = await RequestDataGet(
+        `${this.urlSearch}?text=${this.question}&size=${this.size}&from=${
+          10 * this.page - 9
+        }`
+      );
+    },
+    async page() {
+      this.data = await RequestDataGet(
+        `${this.urlSearch}?text=${this.question}&size=${this.size}&from=${
+          10 * this.page - 9
+        }`
+      );
+    },
+    modalPrepareData() {
+      RequestDataGet(
+        `${this.urlDetail}/package/npm/${this.modalPrepareData.name}@${this.modalPrepareData.version}`
+      ).then((data) => {
+        this.detailModal = data;
+        this.detailModal.data = this.data.objects[this.modalPrepareData.itemId];
+        this.detailModal.modalToggle = this.modalPrepareData.modalToggle;
+      });
+    },
+    data() {
+      this.countItems = +this.data.total;
+    },
+  },
+  methods: {
+    getSearchText(data) {
+      this.question = data;
+    },
+    showModal(data) {
+      this.modalPrepareData = data;
+    },
+    closeModal() {
+      this.modalPrepareData.modalToggle = false;
+      this.detailModal.modalToggle = false;
+    },
+    getPage(page) {
+      this.page = page;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
